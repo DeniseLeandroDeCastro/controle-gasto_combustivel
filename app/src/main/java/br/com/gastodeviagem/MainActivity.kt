51c8672fb4d4f1cff1e205658ec4cf6cf7c32ed8
DeniseLeandroDeCastro <dmaria.natal@gmail.com>
2021-09-05 10:48:16 -0300

    Pequeno app para controle de gastos com combustível

---
+++ app/src/main/java/br/com/gastodeviagem/MainActivity.kt
@@ -0,0 +1,50 @@
+package br.com.gastodeviagem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.NumberFormatException
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonCalculate.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View?) {
+        val id = view?.id
+        if(id == R.id.buttonCalculate) {
+            calculate()
+        }
+    }
+
+    private fun calculate() {
+       if(validationOk()) {
+           try {
+
+               val distance = editDistance.text.toString().toFloat()
+               val price = editPrice.text.toString().toFloat()
+               val autonomy = editAutonomy.text.toString().toFloat()
+
+               val totalValue = (distance * price) / autonomy
+               textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+
+           } catch (nfe: NumberFormatException) {
+               Toast.makeText(this, getString(R.string.informe_valores_validos), Toast.LENGTH_SHORT).show()
+           }
+       } else {
+           Toast.makeText(this, getString(R.string.preencha_todos_os_campos), Toast.LENGTH_SHORT).show()
+       }
+    }
+
+    private fun validationOk(): Boolean {
+        return (editDistance.text.toString()!=""
+                && editPrice.text.toString()!=""
+                && editAutonomy.text.toString()!="")
+    }
+}
